56a5413286779c01288163ffb0de0aa4ebf9abc2
Ahmed Yasser <aaysr201678@gmail.com>
2023-07-28 00:18:29 +0300

    task

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class NewPage extends StatelessWidget {
-  const NewPage({super.key, required this.email});
+class NewPage extends StatefulWidget {
+  const NewPage({
+    super.key,
+  });
   static String id = 'Home';
-  final String email;
+
+  @override
+  State<NewPage> createState() => _NewPageState();
+}
+
+class _NewPageState extends State<NewPage> {
+  Future getEmail() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    email = prefs.getString('email');
+  }
+
+  String? email;
+  @override
+  void initState() {
+    super.initState();
+    getEmail();
+  }
 
   @override
   Widget build(BuildContext context) {
